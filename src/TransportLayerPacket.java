807a6753ac4160faefc2848afeda52b48951345c
Luke Green CS2019 <luke.green.2019@uni.strath.ac.uk>
2021-10-27 16:33:20 +0100

    Update TransportLayerPacket.java

--- src/TransportLayerPacket.java
+++ src/TransportLayerPacket.java
@@ -10,7 +10,21 @@ public class TransportLayerPacket {
     // You may need extra methods
 
     public TransportLayerPacket(TransportLayerPacket pkt) {
-        // complete this method
+        // constructor that creates a copy of passed pkt
+
+        this.seqnum = pkt.getSeqnum();
+        this.acknum = pkt.getAcknum();
+        this.data = pkt.getData();
+
+    }
+
+    public TransportLayerPacket(byte[] msg, int seq, int ack) {
+        // constructor that creates a new pkt
+
+        this.seqnum = seq;
+        this.acknum = ack;
+        this.data = msg;
+
     }
 
     public void setSeqnum(int seqnum) {
@@ -19,6 +33,14 @@ public class TransportLayerPacket {
 
     public void setAcknum(int acknum) {
         this.acknum = acknum;
+    }
+
+    public int getSeqnum() {
+        return seqnum;
+    }
+
+    public int getAcknum() {
+        return acknum;
     }
 
     public byte[] getData() {
