693fc2c8484e34febfc528b5f4cb08fe24745204
Luke Green CS2019 <luke.green.2019@uni.strath.ac.uk>
2021-10-27 16:31:29 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        NetworkSimulator sim = new NetworkSimulator(10, 0.0, 0.0, 10.0, false, 1);
+        NetworkSimulator sim = new NetworkSimulator(5, 0.0, 0.0, 200.0, false, 2);
 
+        Sender sender = new Sender("sender", sim);
         // TODO: Set the sender   (sim.setSender)
-
+        sim.setSender(sender);
         // TODO: Set the receiver (sim.setReceiver)
+        Receiver receiver = new Receiver("receiver",sim);
+        sim.setReceiver(receiver);
 
         sim.runSimulation();
     }
