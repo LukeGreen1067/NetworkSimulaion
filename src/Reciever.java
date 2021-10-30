e6c8f6ae755ec835348f6df66c8e765c4a38d4da
Antrea Constantinou CES2019 <antrea.constantinou.2019@uni.strath.ac.uk>
2021-10-30 15:21:04 +0100

    Update checksum Reciever.java

--- src/Reciever.java
+++ src/Reciever.java
@@ -17,10 +17,33 @@ class Receiver extends TransportLayer {
         name=n;
     }
 
-    public static long getCRC32Checksum(byte[] bytes) {
-        Checksum crc32 = new CRC32();
-        crc32.update(bytes, 0, bytes.length);
-        return crc32.getValue();
+ /*  public static long Checksum(byte[] bytes) {
+       Checksum crc32 = new CRC32();
+       crc32.update(bytes, 0, bytes.length);
+       return crc32.getValue();
+   }*/
+    public static int Checksum(byte[] bytes) {
+
+       int crc = 0xFFFF;
+       int val = 0;
+
+       int len = bytes.length;
+
+       for(int pos = 0; pos< len; pos++){
+           crc ^=(int)(0x00ff & bytes[pos]);
+           for(int i = 8; i != 0; i--){
+               if((crc & 0x0001) != 0){
+                   crc >>=1;
+                   crc ^= 0xA001;
+               }
+               else
+                   crc >>=1;
+           }
+       }
+       val = (crc & 0xff) << 8;
+       val = val +((crc >> 8) & 0xff);
+
+       return val;
     }
 
     public void init() {
@@ -43,9 +66,13 @@ class Receiver extends TransportLayer {
 
 
         byte[] data = pkt.getData();
-        long checksum = getCRC32Checksum(data);
-        long checksuminpkt= pkt.getchecksum();
-        System.out.format("checksum in receive ->"+checksum+" checksiumnpck->"+checksuminpkt+" seq->"+pkt.getSeqnum()+"\n");
+       /* long checksum = Checksum(data);
+        long checksuminpkt= pkt.getchecksum();*/
+       /*byte [] checksum = Checksum(data);
+        byte[] checksuminpkt = pkt.getchecksum();*/
+        int checksum = Checksum(data);
+        int checksuminpkt = pkt.getchecksum();
+        System.out.format(" checksum in receive ->"+checksum+" checksiumnpck->"+checksuminpkt+" seq->"+pkt.getSeqnum()+"\n");
         if ((checksum!=checksuminpkt) || (pkt.getSeqnum() < 0)) {
             // error packet is corrupted. do not send  ack
 
