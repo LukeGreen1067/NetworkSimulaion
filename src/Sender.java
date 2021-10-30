048f3a0d4b1810f0a3e5714b99b358e3254def76
Antrea Constantinou CES2019 <antrea.constantinou.2019@uni.strath.ac.uk>
2021-10-30 15:21:45 +0100

    Update checksum Sender.java

--- src/Sender.java
+++ src/Sender.java
@@ -1,5 +1,7 @@
+/*
 import java.util.zip.CRC32;
 import java.util.zip.Checksum;
+*/
 
 class Sender extends TransportLayer {
 
@@ -14,11 +16,35 @@ int counter=0;
         simulator = sim;
         name=n;
     }
-    public static long getCRC32Checksum(byte[] bytes) {
+/*   public static long Checksum(byte[] bytes) {
         Checksum crc32 = new CRC32();
-        crc32.update(bytes, 0, bytes.length);
-        return crc32.getValue();
+       crc32.update(bytes, 0, bytes.length);
+       return crc32.getValue();
+   }*/
+public static int Checksum(byte[] bytes) {
+
+    int crc = 0xFFFF;
+    int val = 0;
+
+    int len = bytes.length;
+
+    for(int pos = 0; pos< len; pos++){
+        crc ^=(int)(0x00ff & bytes[pos]);
+        for(int i = 8; i != 0; i--){ // loop over each bit
+            if((crc & 0x0001) != 0){
+                crc >>=1;
+                crc ^= 0xA001;
+            }
+            else
+                crc >>=1;
+        }
     }
+    val = (crc & 0xff) << 8;
+    val = val +((crc >> 8) & 0xff);
+
+    return val;
+}
+
     public void init() {
 
         seqnum=0;
@@ -30,9 +56,11 @@ int counter=0;
 
         if (seqnum==acknum) {//if all have been ack then we can send next packet from application
             seqnum++;
-            long checksum = getCRC32Checksum(data);
+         /*   long checksum = Checksum(data);*/
+            /*byte[] checksum =  Checksum(data);*/
+            int checksum =  Checksum(data);
             pkttosend = new TransportLayerPacket(data, seqnum, acknum, checksum);
-            System.out.format("checksum->"+ pkttosend.getchecksum()+" seq->"+pkttosend.getSeqnum()+"\n");
+            System.out.format(" data->"+data +" checksum->"+ pkttosend.getchecksum()+" seq->"+pkttosend.getSeqnum()+"\n");
             simulator.startTimer(this,100.0);
             simulator.sendToNetworkLayer(this, pkttosend);
         } else {
@@ -48,7 +76,7 @@ int counter=0;
     //since we are simulating one way sending the sender will only get ACK packets
     public void rdt_receive(TransportLayerPacket pkt) {
 
-               acknum = pkt.getAcknum();
+        acknum = pkt.getAcknum();
         if (acknum < 0) {
             System.out.format("Sender-> Received corrupted ack. resending\n");
             simulator.stopTimer(this);
@@ -59,15 +87,14 @@ int counter=0;
             simulator.stopTimer(this);
         }
     }
-
     public void timerInterrupt() {
         System.out.format("Sender-> timeOut waiting for ack -> resend pkt with seq="+pkttosend.getSeqnum()+"\n");
-        System.out.format("timeout checksum->"+ pkttosend.getchecksum()+" seq->"+pkttosend.getSeqnum()+" new->"+getCRC32Checksum(pkttosend.getData())+"\n");
+        System.out.format("timeout checksum->"+ pkttosend.getchecksum()+" seq->"+pkttosend.getSeqnum()+ " new checksum->"+Checksum(pkttosend.getData())+"\n");
         simulator.startTimer(this,100.0);
         simulator.sendToNetworkLayer(this, pkttosend);
-      /*  counter++;
+      counter++;
         if (counter>5) System.exit(0);
-*/
+
     }
 
 }
