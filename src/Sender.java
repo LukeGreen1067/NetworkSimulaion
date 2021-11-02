ff3fbce6c685253ee8a111fd81a7dcb4b7718baf
Antrea Constantinou CES2019 <antrea.constantinou.2019@uni.strath.ac.uk>
2021-11-02 23:11:51 +0000

    Update final Sender.java

--- src/Sender.java
+++ src/Sender.java
@@ -1,7 +1,5 @@
-/*
-import java.util.zip.CRC32;
-import java.util.zip.Checksum;
-*/
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class Sender extends TransportLayer {
 
@@ -10,63 +8,85 @@ class Sender extends TransportLayer {
     private int acknum;
     private String name;
     TransportLayerPacket pkttosend;
-int counter=0;
+    private boolean readyToSend = true;
+    private ArrayList<byte[]> queue;
+
     public Sender(String n, NetworkSimulator sim) {
         super(n,sim);
         simulator = sim;
         name=n;
     }
-/*   public static long Checksum(byte[] bytes) {
-        Checksum crc32 = new CRC32();
-       crc32.update(bytes, 0, bytes.length);
-       return crc32.getValue();
-   }*/
-public static int Checksum(byte[] bytes) {
 
-    int crc = 0xFFFF;
-    int val = 0;
-
-    int len = bytes.length;
-
-    for(int pos = 0; pos< len; pos++){
-        crc ^=(int)(0x00ff & bytes[pos]);
-        for(int i = 8; i != 0; i--){ // loop over each bit
-            if((crc & 0x0001) != 0){
-                crc >>=1;
-                crc ^= 0xA001;
-            }
-            else
-                crc >>=1;
+    private byte[] takeFromQueue(){
+        if(queue.size() > 0) {
+            byte nextData[] = queue.get(0);
+            queue.remove(0);
+            return nextData;
         }
+        return null;
     }
-    val = (crc & 0xff) << 8;
-    val = val +((crc >> 8) & 0xff);
 
-    return val;
-}
+
+    public static int genchecksum(byte[] buf) {
+
+        int crc =  0xFFFF;
+        int val = 0;
+
+        int len=buf.length;
+
+        for (int pos = 0; pos < len; pos++) {
+            crc ^= (int)(0x00ff & buf[pos]);
+            for (int i = 8; i != 0; i--) {    // Loop over each bit
+                if ((crc & 0x0001) != 0) {      // If the LSB is set
+                    crc >>= 1;                    // Shift right and XOR 0xA001
+                    crc ^= 0xA001;
+                }
+                else                            // Else LSB is not set
+                    crc >>= 1;                    // Just shift right
+            }
+        }
+
+        val =  (crc & 0xff) << 8;
+        val =  val + ((crc >> 8) & 0xff);
+
+        return val;
+
+    }
 
     public void init() {
 
-        seqnum=0;
-        acknum=0;
+        queue = new ArrayList<>();
+        seqnum=-1;
+        acknum=-1;
+
     }
 
     //send a packet from layer5 to layer3
     public void rdt_send(byte[] data) {
 
-        if (seqnum==acknum) {//if all have been ack then we can send next packet from application
+        if (readyToSend) {
+
+            if(queue.size() > 0) {
+                queue.add(data);
+                data = queue.get(0);
+                queue.remove(0);
+            }
+
+            readyToSend = false;
             seqnum++;
-         /*   long checksum = Checksum(data);*/
-            /*byte[] checksum =  Checksum(data);*/
-            int checksum =  Checksum(data);
+            if (seqnum==2) seqnum=0;
+            int checksum = genchecksum(data);
             pkttosend = new TransportLayerPacket(data, seqnum, acknum, checksum);
-            System.out.format(" data->"+data +" checksum->"+ pkttosend.getchecksum()+" seq->"+pkttosend.getSeqnum()+"\n");
-            simulator.startTimer(this,100.0);
+            //  System.out.format("checksum->"+ pkttosend.getchecksum()+" seq->"+pkttosend.getSeqnum()+"\n");
+            //simulator.sendToApplicationLayer(this,pkttosend.getData());
+            System.out.println("Sender --> Sending packet with seq:"+seqnum);
             simulator.sendToNetworkLayer(this, pkttosend);
+            simulator.startTimer(this,100.0);
+
+
         } else {
-            System.out.format("sender-> got a layer5 packet to send before all previously sent have been ack.\n");
-            System.out.format("sender-> pkt will never be sent.\n");
-            //System.exit(0);
+            System.out.println("Sender --> New data from layer5 added to queue - sender in Stop state.");
+            queue.add(data);
         }
 
 
@@ -78,22 +98,38 @@ public static int Checksum(byte[] bytes) {
 
         acknum = pkt.getAcknum();
         if (acknum < 0) {
-            System.out.format("Sender-> Received corrupted ack. resending\n");
+            System.out.format("Sender-> Received corrupted ack. resending packet with seq:"+seqnum + "\n");
             simulator.stopTimer(this);
-            simulator.startTimer(this, 100.0);
             simulator.sendToNetworkLayer(this, pkttosend);
+            simulator.startTimer(this, 100.0);
+
         } else {
+            readyToSend = true;
             System.out.format("Sender-> Received ack: " + acknum + "\n");
             simulator.stopTimer(this);
+            byte[] queueNext = takeFromQueue();
+            if (queueNext != null) {
+                readyToSend = false;
+                seqnum++;
+                if (seqnum == 2) seqnum = 0;
+                int checksum = genchecksum(queueNext);
+                pkttosend = new TransportLayerPacket(queueNext, seqnum, acknum, checksum);
+                //  System.out.format("checksum->"+ pkttosend.getchecksum()+" seq->"+pkttosend.getSeqnum()+"\n");
+                //simulator.sendToApplicationLayer(this,pkttosend.getData());
+                System.out.println("Sender --> Sending packet with seq:" + seqnum);
+                simulator.sendToNetworkLayer(this, pkttosend);
+                simulator.startTimer(this, 100.0);
+            }
         }
     }
+
     public void timerInterrupt() {
         System.out.format("Sender-> timeOut waiting for ack -> resend pkt with seq="+pkttosend.getSeqnum()+"\n");
-        System.out.format("timeout checksum->"+ pkttosend.getchecksum()+" seq->"+pkttosend.getSeqnum()+ " new checksum->"+Checksum(pkttosend.getData())+"\n");
+        // System.out.format("timeout checksum->"+ pkttosend.getchecksum()+" seq->"+pkttosend.getSeqnum()+" new->"+getCRC32Checksum(pkttosend.getData())+"\n");
+        //simulator.sendToApplicationLayer(this,pkttosend.getData());
         simulator.startTimer(this,100.0);
         simulator.sendToNetworkLayer(this, pkttosend);
-      counter++;
-        if (counter>5) System.exit(0);
+
 
     }
 
